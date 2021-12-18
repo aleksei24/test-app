696d2e43adf56001300dab690dd3af900e1e6af3
aleksei24 <lexa24032@gmail.com>
2021-12-18 10:48:35 +0300

    background colour flipper is done

--- script/script.js
+++ script/script.js
@@ -1,2 +1,22 @@
 // ==================================================================
 console.log();
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
+
+const wrapper = document.querySelector('.wrapper');
+const btnFlip = document.querySelector('#btn-flip');
+const spanColour = document.querySelector('.colour');
+
+btnFlip.addEventListener('click', function () {
+  let hexColour = '#';
+
+  for (let i = 0; i < 6; i++) {
+    hexColour += hex[getRandomNumber()];
+  }
+
+  spanColour.textContent = hexColour;
+  wrapper.style.backgroundColor = hexColour;
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * hex.length);
+}
